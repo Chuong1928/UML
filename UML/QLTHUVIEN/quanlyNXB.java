374f6fe222ebbe13c55e6c82af700e02fc7bd4dc
Chuong1928 <6789tyh@gmail.com>
2020-12-02 01:01:02 +0700

    ahihi

---
+++ UML/QLTHUVIEN/quanlyNXB.java
@@ -0,0 +1,46 @@
+
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @created 21-Oct-2019 2:01:26 PM
+ */
+public class NhaXuatBan {
+
+	private String MaNXB;
+	private int NamthanhLAP;
+	private String TenNXB;
+
+	public NhaXuatBan(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param maNXB
+	 * @param tenNXB
+	 * @param namThanhLap
+	 */
+	public NhaXuatBan(String maNXB, String tenNXB, int namThanhLap){
+
+	}
+
+	public String getMaNhaXuatBan(){
+		return "";
+	}
+
+	public String getThongTin(){
+		return "";
+	}
+
+	/**
+	 * 
+	 * @param stMaNhaXuatBanMoi
+	 */
+	public void setMaNhaXuatBan(String stMaNhaXuatBanMoi){
+
+	}
+}//end NhaXuatBan
